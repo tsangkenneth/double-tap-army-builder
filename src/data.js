06b5e62145f49c027eb21de2f8ed57fe10dee042
Kenneth Tsang <tsangkenneth@users.noreply.github.com>
2023-05-10 14:58:20 -0700

    Add rome (#15)
    * Install Rome
    
    * Remove unused .gitignore setting
    
    * Add rome.json
    
    * Lint and format
    
    * Fix Vite preview

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 export const specialRules = {
   ambush: {
     name: 'Ambush',
-    cost: 2
+    cost: 2,
   },
   armored: {
     name: 'Armored',
-    cost: 4
+    cost: 4,
   },
   athletic: {
     name: 'Athletic',
-    cost: 1
+    cost: 1,
   },
   camouflage: {
     name: 'Camoflage',
-    cost: 4
+    cost: 4,
   },
   doctor: {
     name: 'Doctor',
@@ -21,97 +21,97 @@ export const specialRules = {
   },
   engineer: {
     name: 'Engineer',
-    cost: 6
+    cost: 6,
   },
   fast: {
     name: 'Fast',
-    cost: 2
+    cost: 2,
   },
   fireAmmo: {
     name: 'Fire Ammo',
-    cost: 4
+    cost: 4,
   },
   flying: {
     name: 'Flying',
-    cost: 3
+    cost: 3,
   },
   grenade: {
     name: 'Grenade',
-    cost: 1
+    cost: 1,
   },
   hacker: {
     name: 'Hacker',
-    cost: 2
+    cost: 2,
   },
   infiltrate: {
     name: 'Infiltration',
-    cost: 2
+    cost: 2,
   },
   piercingAmmo: {
     name: 'Piercing Ammo',
-    cost: 2
+    cost: 2,
   },
   poisonGear: {
     name: 'Poison Gear',
-    cost: 2
+    cost: 2,
   },
   regeneration: {
     name: 'Regneration',
-    cost: 4
+    cost: 4,
   },
   scout: {
     name: 'Scout',
-    cost: 2
+    cost: 2,
   },
-  sixthSense:{
+  sixthSense: {
     name: 'Sixth Sense',
-    cost: 2
+    cost: 2,
   },
   spotter: {
     name: 'Spotter',
-    cost: 2
+    cost: 2,
   },
   tough: {
     name: 'Tough',
-    cost: 12
-  }
-}
+    cost: 12,
+  },
+};
 
 export const weapons = {
   knife: {
     name: 'Knife',
-    cost: 1
+    cost: 1,
   },
   sword: {
     name: 'Sword',
-    cost: 4
+    cost: 4,
   },
   pistol: {
     name: 'Pistol',
-    cost: 3 
+    cost: 3,
   },
   shotgun: {
     name: 'Shotgun',
-    cost: 3
+    cost: 3,
   },
   smg: {
     name: 'SMG',
-    cost: 4
+    cost: 4,
   },
   carbine: {
     name: 'Carbine',
-    cost: 5
+    cost: 5,
   },
   rifle: {
     name: 'Rifle',
-    cost: 5
+    cost: 5,
   },
   lmg: {
     name: 'LMG',
-    cost: 6
+    cost: 6,
   },
   sniperRifle: {
     name: 'Sniper Rifle',
-    cost: 6
-  }
-}
+    cost: 6,
+  },
+};
